959d827b9ff0bc41079b06448df3c8943a2d2a75
Ryu <goskn88006@yahoo.co.jp>
2022-02-13 15:35:47 +0900

    アクションとビューファイルnewとindexを作成

---
+++ app/controllers/propertys_controller.rb
@@ -0,0 +1,49 @@
+class PropertysController < ApplicationController
+  before_action :set_property, only: [:show, :edit, :update, :destroy]
+  def index
+    @propertys=Property.all
+   end
+   def new
+     @property=Property.new
+   end
+   def confirm
+     @property=Property.new(property_params)
+     render :new if @property.invalid?
+   end
+   def create
+    @property = property.new(property_params)
+    if params[:back]
+      render :new
+    else
+      if @property.save
+        redirect_to propertys_path, notice: "新しい物件を登録しました!"      
+      else
+        render :new
+      end
+    end
+  end
+   
+   def show
+   end
+   def edit
+   end
+   def update
+    if @property.update(property_params)
+     redirect_to propertys_path, notice: "物件情報を編集しました！"
+    else
+     render :edit
+    end
+   end
+   def destroy
+     @property.destroy
+     redirect_to propertys_path, notice:"物件を削除しました！"
+   end
+   private
+   def property_params
+     params.require(:property).permit(:property_name,:rent,:address,:age,:memo)
+   end
+   def set_property
+     @property = property.find(params[:id])
+   end
+ end
+ 
