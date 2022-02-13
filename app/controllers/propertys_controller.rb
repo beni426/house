0e70aa129a09da82ac8e6784f00cf15711ff40d4
Ryu <goskn88006@yahoo.co.jp>
2022-02-13 21:42:13 +0900

    validationをつける

--- app/controllers/propertys_controller.rb
+++ app/controllers/propertys_controller.rb
@@ -1,10 +1,12 @@
 class PropertysController < ApplicationController
   before_action :set_property, only: [:show, :edit, :update, :destroy]
+  before_action :set_nearest_stations, only: [:show, :edit, :update]
   def index
     @propertys=Property.all
    end
    def new
      @property=Property.new
+     2.times { @property.nearest_stations.build }
    end
    def create
     @property = Property.new(property_params)
@@ -14,19 +16,23 @@ class PropertysController < ApplicationController
       if @property.save
         redirect_to propertys_path, notice: "新しい物件を登録しました!"      
       else
+        2.times { @property.nearest_stations.build }
         render :new
       end
     end
   end
    
    def show
+    binding.pry
    end
    def edit
+     @property.nearest_stations.build
    end
    def update
     if @property.update(property_params)
      redirect_to propertys_path, notice: "物件情報を編集しました！"
     else
+      @property.nearest_stations.build
      render :edit
     end
    end
@@ -36,10 +42,21 @@ class PropertysController < ApplicationController
    end
    private
    def property_params
-     params.require(:property).permit(:property_name,:rent,:address,:age,:memo)
+   
+     params.require(:property).permit(
+       :property_name,
+       :rent,
+       :address,
+       :age,
+       :memo,
+       nearest_stations_attributes:  %i[ route_name station time ],
+      )
    end
    def set_property
      @property = Property.find(params[:id])
+   end
+   def set_nearest_stations
+    @nearest_stations=@property.nearest_stations
    end
  end
  
--- app/models/nearest_station.rb
+++ app/models/nearest_station.rb
@@ -1,3 +1,6 @@
 class NearestStation < ApplicationRecord
   belongs_to :property
+  validates :route_name, presence: true, length: { maximum: 50 }
+  validates :station, presence: true, length: { maximum: 50 }
+  validates :time, presence: true
 end
