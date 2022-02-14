dbdf093ef1925d9cb2895572549adc0f6acad1da
Ryu <goskn88006@yahoo.co.jp>
2022-02-14 21:32:24 +0900

    最寄り駅の情報ない時物件情報の登録できるように設定

--- app/models/nearest_station.rb
+++ app/models/nearest_station.rb
@@ -1,6 +1,6 @@
 class NearestStation < ApplicationRecord
   belongs_to :property
-  validates :route_name, presence: true, length: { maximum: 50 }
-  validates :station, presence: true, length: { maximum: 50 }
-  validates :time, presence: true
+  validates :route_name, presence: false, length: { maximum: 50 }
+  validates :station, presence: false, length: { maximum: 50 }
+  validates :time, presence: false
 end
